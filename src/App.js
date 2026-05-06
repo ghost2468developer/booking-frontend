4c5c6a777be3e44d1e82613e00e082ccea45bdfd
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-05-06 17:29:42 +0200

    feat: user update correct structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import ProtectedRoute from "./components/ProtectedRoute"
+
 import Login from "./auth/Login"
 import Register from "./auth/Register"
+
+import UserDashboard from "./dashboard/UserDashboard"
 import MechanicsList from "./mechanics/MechanicsList"
-import MyBookings from "./bookings/MyBookings"
 import CreateBooking from "./bookings/CreateBooking"
 import Bookings from "./bookings/Bookings"
-import Navbar from "./components/Navbar"
-import UserDashboard from "./dashboard/UserDashboard"
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
+        {/* AUTH */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/mechanics" element={<MechanicsList />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/book" element={<CreateBooking />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
+
+        {/* USER FLOW (PROTECTED) */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/mechanics"
+          element={
+            <ProtectedRoute>
+              <MechanicsList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/book"
+          element={
+            <ProtectedRoute>
+              <CreateBooking />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/bookings"
+          element={
+            <ProtectedRoute>
+              <Bookings />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
